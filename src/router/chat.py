b23652f8b624a519545a15ef0cc62eaf5ddc33a1
Shin Hyun <kyaryunha@naver.com>
2023-11-26 05:33:10 +0900

    error handlig

--- src/router/chat.py
+++ src/router/chat.py
@@ -8,45 +8,54 @@ router = Blueprint('chat', __name__, url_prefix='/chat')
 
 @router.route('create', methods=['POST'])
 def create_chat():
-    data = request.get_json()
-    chat = Chat(
-        video_name=data['video_name'],
-        content=data['content'],
-    )
-    db.session.add(chat)
-    db.session.commit()
-    return jsonify({
-        'message': 'success'
-    }), 200
-
+    try:
+        data = request.get_json()
+        chat = Chat(
+            video_name=data['video_name'],
+            content=data['content'],
+        )
+        db.session.add(chat)
+        db.session.commit()
+        return jsonify({
+            'message': 'success'
+        }), 200
+    except Exception as e:
+        return jsonify({
+            'message': str(e)
+        }), 500
 
 @router.route('list', methods=['GET'])
 def chats():
-    video_name = request.args.get('video_name', "")
+    try:
+        video_name = request.args.get('video_name', "")
 
-    # video_name none 일 경우
-    if video_name == "":
-        chats = Chat\
-            .query \
-            .order_by(Chat.created_at.desc()).all()
+        # video_name none 일 경우
+        if video_name == "":
+            chats = Chat\
+                .query \
+                .order_by(Chat.created_at.desc()).all()
 
-    else:
-        chats = Chat \
-            .query \
-            .filter_by(video_name=video_name) \
-            .order_by(Chat.created_at.desc()).all()
+        else:
+            chats = Chat \
+                .query \
+                .filter_by(video_name=video_name) \
+                .order_by(Chat.created_at.desc()).all()
 
-    if chats is None:
-        chats = []
+        if chats is None:
+            chats = []
 
-    result = []
-    for chat in chats:
-        result.append({
-            'video_name': chat.video_name,
-            'content': chat.content,
-            'created_at': chat.created_at.strftime('%Y-%m-%d %H:%M:%S')
-        })
+        result = []
+        for chat in chats:
+            result.append({
+                'video_name': chat.video_name,
+                'content': chat.content,
+                'created_at': chat.created_at.strftime('%Y-%m-%d %H:%M:%S')
+            })
 
-    return jsonify({
-        'chats': result
-    }), 200
+        return jsonify({
+            'chats': result
+        }), 200
+    except Exception as e:
+        return jsonify({
+            'message': str(e)
+        }), 500
--- src/router/video_list.py
+++ src/router/video_list.py
@@ -7,31 +7,41 @@ router = Blueprint('video_list', __name__, url_prefix='/video_list')
 
 @router.route('/', methods=['GET'])
 def get_video_list():
-    list = s3.list_objects_v2(Bucket='furiosa-video', Prefix='convert/')
-    if 'Contents' not in list:
+    try:
+        list = s3.list_objects_v2(Bucket='furiosa-video', Prefix='convert/')
+        if 'Contents' not in list:
+            return {
+                'list': [],
+                'count': 0
+            }
+        result = []
+        for item in list['Contents']:
+            print(item)
+            if 'Key' in item:
+                result.append({
+                    'name': item['Key'].split('/')[1],
+                    'created_at': item['LastModified'].strftime('%Y-%m-%d %H:%M:%S')
+                })
         return {
-            'list': [],
-            'count': 0
+            'list': result,
+            'count': len(result)
         }
-    result = []
-    for item in list['Contents']:
-        print(item)
-        if 'Key' in item:
-            result.append({
-                'name': item['Key'].split('/')[1],
-                'created_at': item['LastModified'].strftime('%Y-%m-%d %H:%M:%S')
-            })
-    return {
-        'list': result,
-        'count': len(result)
-    }
+    except Exception as e:
+        return {
+            'message': str(e)
+        }, 500
 
 
 @router.route('/clear_all', methods=['GET'])
 def clear_all():
-    list = s3.list_objects_v2(Bucket='furiosa-video', Prefix='convert/')['Contents']
-    for item in list:
-        s3.delete_object(Bucket='furiosa-video', Key=item['Key'])
-    return {
-        'message': 'All video list cleared'
-    }
+    try:
+        list = s3.list_objects_v2(Bucket='furiosa-video', Prefix='convert/')['Contents']
+        for item in list:
+            s3.delete_object(Bucket='furiosa-video', Key=item['Key'])
+        return {
+            'message': 'All video list cleared'
+        }
+    except Exception as e:
+        return {
+            'message': str(e)
+        }, 500
