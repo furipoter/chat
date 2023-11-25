48697bf5ccef0c3da59b5239f1f35eca8e881c08
Shin Hyun <kyaryunha@naver.com>
2023-11-26 05:28:22 +0900

    add chat

--- src/db/chat.py
+++ src/db/chat.py
@@ -3,5 +3,6 @@ from src.db import db
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    video_name = db.Column(db.String(64), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
